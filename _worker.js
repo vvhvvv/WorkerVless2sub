3beb59681ebc1c10c0aa953f76aae56cb9daa881
vvhvvv <112534104+vvhvvv@users.noreply.github.com>
2024-07-28 23:55:58 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -5,8 +5,8 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'yx1.nssb.vip#优选1',
+	'yx2.nssb.vip#优选2',
 ];
 
 // 设置优选地址api接口
